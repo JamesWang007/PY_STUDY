5c4a41a3eec84b5216e2ceccb776f0a3f51cea3d
JamesWang007 <wanghaiyang20@gmail.com>
2018-12-28 00:39:07 -0800

    .

--- cv/cv02.py
+++ cv/cv02.py
@@ -1,17 +1,35 @@
 # -*- coding: utf-8 -*-
+'''
+camera 
+load frames from VideoCapture and display them through imshow()
+
+'''
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-#       0
-img = cv2.imread ("images\Lenna.jpg", cv2.IMREAD_GRAYSCALE)
-#IMREAD_COLOR = 1
-#IMREAD_UNCHANGED = -1
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
-#cv2.imshow('image', img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    out.write(frame)
+    
+    cv2.imshow('frame', frame)
+    cv2.imshow('gray', gray)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+    
 
-plt.imshow(img, cmap='gray')
+
+
+
+
 
