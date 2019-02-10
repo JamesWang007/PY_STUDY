1a3c5bd81324dc547bdb6bfc31c3f1c8a05d67a0
Haiyang Wang <haiyangwang@Haiyangs-MacBook-Pro.local>
2019-02-09 16:09:03 -0800

    ss

--- cv/cv01.py
+++ cv/cv01.py
@@ -12,9 +12,32 @@ img = cv2.imread ("images/Lenna.png", cv2.IMREAD_GRAYSCALE)
 #cv2.imshow('image', img)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
-
+# nothing ~haha
 plt.imshow(img, cmap='gray')
 plt.plot([50, 100], [80, 100], 'c', linewidth = 5)
 plt.show()
 
 cv2.imwrite('images/myLenna.png', img)
+
+# --- also work
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+img = cv2.imread('images/Lenna.png', -1)
+rows,cols = img.shape[:2]
+
+M = cv2.getRotationMatrix2D((cols/2,rows/2),45,1)
+dst = cv2.warpAffine(img,M,(cols,rows))
+
+plt.imshow(M)
+plt.show()    
+
+plt.imshow(img)
+plt.show()    
+
+plt.imshow(dst)
+plt.show()
